68acdff951e961c35c1189f728573b997c7ae3f3
Atasay Gokkaya <atasay@wanelo.com>
2014-11-03 17:19:30 -0800

    Set graphite-web timezone to pacific

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@wanelo.com'
 license          'MIT'
 description      'Install graphite on SmartOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.0.4'
 
 supports 'smartos'
 
